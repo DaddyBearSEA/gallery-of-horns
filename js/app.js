84cc1e1aeb5b7f3a196f184d7697c20bf5caa9fd
Cody Carpenter <cody_carpenter2222@yahoo.com>
2020-09-30 23:23:59 -0700

    not complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log('hello world');
+let $animalsTemplate = $('.animalsTemplate');
+let $horns = $('.animals');
+
+let imgArr = [];
+
+$.ajax('./data/page-1.json', { method: 'get', datatype: 'json' })
+  .then(potato => {
+    potato.forEach(animalsVal => {
+      new Hornsgallery(animalsVal).renderJquery();
+    });
+  });
+function Hornsgallery(src) {
+  this.src = src.image_url;
+  // this.title = title;
+  // this.description = description;
+  // this.keyword = keyword;
+  // this.horns = horns;
+  imgArr.push(this);
+}
+
+Hornsgallery.prototype.renderJquery = function () {
+  let $newUrl = $animalsTemplate.clone();
+  $animalsTemplate.find('img').attr('src', this.src);
+  // $newUrl.text(imageurl.image_url);
+  $horns.append($newUrl);
+
+};
+
+
+
