516cae95b2151cc90ea6cfcaf9ff4efb55c93c2a
Cody Carpenter <cody_carpenter2222@yahoo.com>
2020-10-01 20:20:10 -0700

    images now show on page

--- js/app.js
+++ js/app.js
@@ -11,22 +11,19 @@ $.ajax('./data/page-1.json', { method: 'get', datatype: 'json' })
       new Hornsgallery(animalsVal).renderJquery();
     });
   });
+
 function Hornsgallery(src) {
   this.src = src.image_url;
-  // this.title = title;
-  // this.description = description;
-  // this.keyword = keyword;
-  // this.horns = horns;
+  this.title = src.title;
+  this.description = src.description;
+  this.keyword = src.keyword;
+  this.hoyrns = src.horns;
   imgArr.push(this);
 }
 
 Hornsgallery.prototype.renderJquery = function () {
-  let $newUrl = $animalsTemplate.clone();
-  $animalsTemplate.find('img').attr('src', this.src);
-  // $newUrl.text(imageurl.image_url);
-  $horns.append($newUrl);
-
+  let $newUrl = $horns.clone();
+  $newUrl.find('img').attr('src', this.src);
+  // $newUrl.text('test');
+  $animalsTemplate.append($newUrl);
 };
-
-
-
